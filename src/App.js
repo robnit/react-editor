95683cb6ef8c8b834bcaa6ab98800bb442699bad
Robin Miller <rmillerpdx@gmail.com>
2017-11-07 12:19:47 -0800

    get text to change using react sorcery

--- src/App.js
+++ src/App.js
@@ -3,16 +3,60 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      outputText: 'woof'
+    };
+  }
+
+  handleTextChange(value) {
+    this.setState({ outputText: value });
+  }
+
   render() {
+    const { outputText } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Controls outputText={outputText}
+          onTextChange={outputText => this.handleTextChange(outputText)}/>
+        <Text outputText={outputText} />
+      </div>
+    );
+  }
+
+}
+
+class Controls extends Component {
+  render() {
+    const { outputText, onTextChange } = this.props;
+
+    return (
+      <div>
+        <label>
+          Insert Text Here:
+          <input name="outputText" value={outputText} onChange={({ target }) => onTextChange(target.value)}/>
+        </label>
+      </div>
+    );
+  }
+}
+
+class Text extends Component {
+  render() {
+
+    const { outputText } = this.props;
+
+    return (
+      <div>
+        <div>
+          <label>
+            <span>{outputText}</span>
+          </label>
+        </div>
+
       </div>
     );
   }
